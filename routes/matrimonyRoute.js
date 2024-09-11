77cb34976a7d0bb98cc7e5dbded2318e564deb0b
Divyani Chhapre <divyanichhapre163@gmail.com>
2024-09-11 12:38:04 +0530

    pst

--- routes/matrimonyRoute.js
+++ routes/matrimonyRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const matrimonyController = require("../controller/matrimonyController");
+// const matrimonyController = require("../controller/matrimonyController");
 
-router.get("/",matrimonyController );
-router.get("/:id",matrimonyController);
-router.post("/", matrimonyController);
-router.delete("/:id",matrimonyController);
+// router.get("/",matrimonyController );
+// router.get("/:id",matrimonyController);
+// router.post("/", matrimonyController);
+// router.delete("/:id",matrimonyController);
 
 module.exports = router;
